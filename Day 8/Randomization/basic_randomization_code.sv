e5bbc86405dbe46479bc2362972e4e6d1b015022
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-01-22 13:14:20 +0530

    Create basic_randomization_code.sv

---
+++ Day 8/Randomization/basic_randomization_code.sv
@@ -0,0 +1,23 @@
+// randomization basic code
+
+class random_ex;
+  
+  rand bit [2:0]a;         // rand keyword used here randomizes possible values for given variable
+  rand bit [2:0]b;
+  
+endclass
+
+module random_example;
+ 
+    random_ex r;
+  
+  initial begin
+    
+    r = new();
+    
+    repeat(10) begin
+      r.randomize();
+      $display(" a = %0d, b = %0d", r.a,r.b);
+    end
+    end
+endmodule
