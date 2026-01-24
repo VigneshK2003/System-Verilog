574d0f532789a18cb7a1dcb8804a9c1d1004a5e2
Vigneshwaran K <vigneshkaruppasamy2003@gmail.com>
2026-01-24 16:03:18 +0530

    Update if_else_constraint.sv

--- Day 8/Constraints/if_else_constraint.sv
+++ Day 8/Constraints/if_else_constraint.sv
@@ -4,11 +4,11 @@ class if_else_const;
   rand bit en;
   rand bit [7:0]a;
   
-  constraint if_ctr { 
-              if(en)
-                a inside {[100:150]}; 
-              else
-                a == 0;
+  constraint if_ctr {          
+          if(en)                                   // if-else constraint
+               a inside {[100:150]}; 
+          else
+               a == 0;
              };
 endclass
 
